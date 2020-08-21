e75ce694db9254cb4ebc8e00bdf29d3f6d8ecdc1
William Pannell <lpann5@gmail.com>
2020-08-21 00:02:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @reversed = params[:name].reverse
+    "#{@reversed}"
+  end
+
+  get '/square/:number' do
+    erb :reversename
+  end
+
+  get '/say/:number/:phrase' do
+    erb :reversename
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    erb :reversename
+  end
+
+  get '/:operation/:number1/:number2' do
+    erb :reversename
+  end
 
 end
