a81f475fd8d8c75cbaf791662442eea13d88e2dd
William Pannell <lpann5@gmail.com>
2020-08-21 00:22:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,8 @@ class App < Sinatra::Base
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
-    @answer = params[:word1] + ' ' + params[:word2] + ' ' + params[:word3] + ' ' + params[:word4] + ' ' + params[:word5]
+    @answer = params[:word1] + ' ' + params[:word2] + ' ' + params[:word3] + ' ' + params[:word4] + ' ' + params[:word5] + '.'
+    "#{@answer}"
   end
 
   get '/:operation/:number1/:number2' do
